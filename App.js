7786a1ba97a3b6a0061dcddcf0baca3a32c60cab
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-07 22:49:01 +0530

    App foundation done

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Sample = () => {
+    return(
+        <h1>Midhun k paniker</h1>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<Sample/>)
