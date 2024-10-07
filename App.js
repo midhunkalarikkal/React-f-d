04179e7685a271465f82153da56558bd6de203e4
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-08 01:45:25 +0530

    header component designed

--- App.js
+++ App.js
@@ -1,13 +1,74 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const Sample = () => {
-    return(
-        <h1>Midhun k paniker</h1>
-    )
-}
+/**
+ * Header
+ *   -Logo
+ *   -Nav items
+ * Body
+ *   -Search bar with button
+ *   -Restaurant card container
+ *       -Restaurant Card
+ *           -Card image
+ *           -Name of res, Star rating, Cuisine, Delivery time
+ * Footer
+ *   -Coyright
+ *   -Links
+ *   -Address
+ *   -Contact
+ **/
 
+const Header = () => {
+  return (
+    <nav className="header">
+      <div className="logo-container">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVermuyA5zuSvADyNPet-giCboNoUHK52G9w&s"
+          alter=""
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <a href="" className="nav-item">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="" className="nav-item">
+              Explore
+            </a>
+          </li>
+          <li>
+            <a href="" className="nav-item">
+              Cart
+            </a>
+          </li>
+          <li>
+            <a href="" className="nav-item">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="" className="nav-item">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
-root.render(<Sample/>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
