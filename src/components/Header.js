a6138cd73881cf91d445335f4af07ba0a62d2a81
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-19 21:06:47 +0530

    navbar design done

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,14 @@ const Header = () => {
   const [btnName, setBtnName] = useState("Login");
   const onlineStatus = useOnlineStatus()
   return (
-    <nav className="header">
-      <div className="logo-container">
-        <img src={LOGO} alter="" className="logo" />
+    <nav className="flex p-3 bg-slate-100">
+      <div className="flex w-1/4 justify-center items-center">
+        <h4 className="px-5 text-lg font-bold underline decoration-orange-500 underline-offset-4">Bhookad Buzz</h4>
+        <img src={LOGO} alter="" className="w-14" />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>
+      <div className=" flex w-3/4 items- justify-end">
+        <ul className="flex w-1/2 justify-around items-center  ">
+          <li className="cursor-pointer">
             {
               onlineStatus === true ? (
                 "🟢 Online"
@@ -22,24 +23,24 @@ const Header = () => {
               )
             }
           </li>
-          <li>
-            <Link to='/' className="nav-item">
+          <li className="">
+            <Link to='/' className="nav-item hover:underline hover:underline-offset-4 hover:decoration-orange-400">
               Home
             </Link>
           </li>
-          <li>
-            <Link to="/about" className="nav-item">
+          <li className="">
+            <Link to="/about" className="nav-item hover:underline hover:underline-offset-4 hover:decoration-orange-400">
               About
             </Link>
           </li>
-          <li>
-            <Link to="/contact" className="nav-item">
+          <li className="">
+            <Link to="/contact" className="nav-item hover:underline hover:underline-offset-4 hover:decoration-orange-400">
               Contact
             </Link>
           </li>
           <li>
             <button
-              className="login-btn"
+              className="login-btn bg-orange-400 py-1 px-4 rounded-md text-yellow-50"
               onClick={() => {
                 btnName === "Login"
                   ? setBtnName("Logout")
