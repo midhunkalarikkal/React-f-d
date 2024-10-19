6a54b48d3f518320dadbd906724b843e2e752f29
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-20 04:18:35 +0530

    Restaurant menu card items list designed

---
+++ src/components/ItemList.js
@@ -0,0 +1,22 @@
+import { CDN_LINK } from "../utils/constants";
+const ItemList = ({ item }) => {
+  console.log(item);
+  return (
+    <>
+      {item.map((item) => 
+        <div className="flex justify-between p-3 bg-slate-100 border-b-2 mt-1">
+          <div className="flex flex-col justify-center w-9/12">
+            <h3 className="font-bold text-md">{item.card.info.name}</h3>
+            <h3>-₹ {item.card.info.price / 100}</h3>
+            <p className="text-xs">{item.card.info.description}</p>
+          </div>
+          <div className="flex justify-center w-3/12">
+            <img src={CDN_LINK + item.card.info.imageId} alt="" className="w-28 object-fill rounded-lg" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ItemList;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,16 +1,17 @@
-const RestaurantCategory = ({ data }) => {
-  console.log(data);
-  return (
-    <div className="mt-2">
-      <div className="flex justify-between bg-slate-200 p-2">
-        <div className="font-bold text-lg">
-          {data.title} ({data.itemCards.length})
-        </div>
-        <div>⬇️</div>
-      </div>
-      <siv className="p-3">
+import ItemList from "./ItemList";
 
-      </siv>
+const RestaurantCategory = ({ data }) => {
+  return (
+    <div>
+      <div className="mt-2">
+        <div className="flex justify-between p-2 bg-slate-200">
+          <div className="font-bold text-lg">
+            {data.title} ({data.itemCards.length})
+          </div>
+          <div>⬇️</div>
+        </div>
+        <ItemList item={data.itemCards} />
+      </div>
     </div>
   );
 };
