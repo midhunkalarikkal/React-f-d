57841daa96d6109251671ca0212639398bbccf3b
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-12-06 12:56:10 +0530

    Body component updated for the alert and search consistency

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,6 +21,10 @@ const Body = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    searchInputText === "" ? setFilteredRestaurant(listOfRestaurant) : ""
+  })
+
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.5276416&lng=76.2144349&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
