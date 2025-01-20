74fc4b6ae0e3df26c5e7aada5dd6dddc22dfdd34
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-21 03:17:02 +0530

    payment page design updated razorpay payment implemented payment successs div addedd and confetti added

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
-
+import { useNavigate } from "react-router-dom";
+import { clearCart } from "../utils/cartSlice";
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 
 const Payment = () => {
+  const cartItems = useSelector((store) => store?.cart?.items);
+  let [cartTotal, setCartTotal] = useState(0);
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  const [showSuccessDiv, setSuccessDiv] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { width, height } = useWindowSize();
 
-  const [ formData, setFormData ] = useState({
+  useEffect(() => {
+    if (cartItems) {
+      const total = cartItems.reduce((acc, cv) => acc + cv.price, 0);
+      setCartTotal(total / 100);
+    }
+
+    const itemsCount = cartItems.length;
+    setCartItemsCount(itemsCount);
+  }, [cartItems]);
+
+  const [formData, setFormData] = useState({
     name: "",
     address: "",
     phone: "",
@@ -15,21 +36,21 @@ const Payment = () => {
     const { id, value } = e.target;
     setFormData({
       ...formData,
-      [id] : value,
-    })
-  }
+      [id]: value,
+    });
+  };
 
   const validateForm = () => {
     const nameRegex = /^[a-zA-Z\s]{3,}$/;
     const phoneRegex = /^[6-9]\d{9}$/;
-    const addressRegex = /^.{10,}$/; // Minimum 10 characters for address
+    const addressRegex = /^.{10,}$/;
 
     const name = formData.name;
     const address = formData.address;
     const phone = formData.phone;
     const city = formData.city;
 
-    if(!name || !address || !phone || !city){
+    if (!name || !address || !phone || !city) {
       toast.error("Please fill the address form");
       return false;
     }
@@ -55,24 +76,35 @@ const Payment = () => {
 
   const handlePayment = () => {
     if (validateForm()) {
-      // Show Razorpay Payment Gateway
       const options = {
-        key: process.env.RAZORPAY_KEY_ID, // Replace with your Razorpay key
-        amount: 11500, // Amount in paisa (₹115.00)
+        key: process.env.RAZORPAY_KEY_ID,
+        amount: (cartTotal + 50) * 100,
         currency: "INR",
-        name: "Your Store Name",
+        name: "TasteTown",
         description: "Test Transaction",
-        handler: (response) => {
+        handler: () => {
           toast.success("Payment Successful!");
-          console.log(response);
+          dispatch(clearCart());
+          setSuccessDiv(true);
+          setTimeout(() => {
+            navigate("/");
+          },5000);
+        },
+        modalClose: () => {
+          toast.error("Payment Failed! Please try again.");
         },
         prefill: {
           name: formData.name,
-          email: "user@example.com", // You can add an email field if required
           contact: formData.phone,
         },
         theme: {
-          color: "#3399cc",
+          color: "#f97316",
+          logo: "https://cdn-icons-png.flaticon.com/512/3027/3027212.png",
+        },
+        external: {
+          error: (error) => {
+            toast.error("Payment failed due to an error! Please try again.");
+          },
         },
       };
 
@@ -82,100 +114,138 @@ const Payment = () => {
   };
 
   return (
-    <div className="md:flex md:justify-between w-full md:w-8/12 m-auto p-6">
-      {/* Left form */}
-      <div className="w-full md:w-6/12 bg-gray-50 p-6 rounded-lg leftdiv shadow-md">
-        <h2 className="text-2xl font-semibold mb-4 text-gray-800">Address Details</h2>
-        <form className="space-y-4">
-          <div>
-            <label htmlFor="name" className="block text-gray-600 mb-2">
-              Full Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              value={formData.name}
-              onChange={handleInputChange}
-              className="w-full p-1 md:p-2 border border-gray-300 rounded-md"
-              placeholder="Enter your full name"
-            />
+    <>
+      {!showSuccessDiv && (
+    <div className="md:grid md:grid-cols-2 gap-6 w-full max-w-7xl m-auto p-6">
+          <div className="bg-gray-50 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+              Address Details
+            </h2>
+            <form className="space-y-4">
+              <div>
+                <label htmlFor="name" className="block text-gray-600 mb-2">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="Enter your full name"
+                />
+              </div>
+              <div>
+                <label htmlFor="address" className="block text-gray-600 mb-2">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  value={formData.address}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="Enter your address"
+                />
+              </div>
+              <div>
+                <label htmlFor="phone" className="block text-gray-600 mb-2">
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="Enter your phone number"
+                />
+              </div>
+              <div>
+                <label htmlFor="city" className="block text-gray-600 mb-2">
+                  City
+                </label>
+                <input
+                  type="text"
+                  id="city"
+                  value={formData.city}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  placeholder="Enter your city"
+                />
+              </div>
+            </form>
           </div>
-          <div>
-            <label htmlFor="address" className="block text-gray-600 mb-2">
-              Address
-            </label>
-            <input
-              type="text"
-              id="address"
-              value={formData.address}
-              onChange={handleInputChange}
-              className="w-full p-1 md:p-2 border border-gray-300 rounded-md"
-              placeholder="Enter your address"
-            />
-          </div>
-          <div>
-            <label htmlFor="phone" className="block text-gray-600 mb-2">
-              Phone Number
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              value={formData.phone}
-              onChange={handleInputChange}
-              className="w-full p-1 md:p-2 border border-gray-300 rounded-md"
-              placeholder="Enter your phone number"
-            />
-          </div>
-          <div>
-            <label htmlFor="city" className="block text-gray-600 mb-2">
-              City
-            </label>
-            <input
-              type="text"
-              id="city"
-              value={formData.city}
-              onChange={handleInputChange}
-              className="w-full p-1 md:p-2 border border-gray-300 rounded-md"
-              placeholder="Enter your city"
-            />
-          </div>
-        </form>
-      </div>
 
-      {/* Payment */}
-      <div className="w-full mt-2 md:mt-0 md:w-5/12 bg-gray-50 p-6 rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Order Summary</h2>
-        <div className="flex justify-between mb-4">
-          <p className="text-gray-600">Items Total:</p>
-          <p className="font-semibold text-gray-800">₹ 120.00</p>
-        </div>
-        <div className="flex justify-between mb-4">
-          <p className="text-gray-600">Delivery Fee:</p>
-          <p className="font-semibold text-gray-800">₹ 5.00</p>
-        </div>
-        <div className="flex justify-between mb-4">
-          <p className="text-gray-600">Discount:</p>
-          <p className="font-semibold text-gray-800">-₹ 00.00</p>
-        </div>
-        <div className="border-t-2 border-gray-300 mt-4 pt-4 flex justify-between">
-          <p className="font-semibold text-lg text-gray-800">Total:</p>
-          <p className="font-semibold text-lg text-gray-800">₹ 115.00</p>
-        </div>
+          <div className="bg-gray-50 p-6 rounded-lg shadow-md mt-2 md:mt-0">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+              Order Summary
+            </h2>
+            <div className="flex justify-between mb-4">
+              <p className="text-gray-600">Items Count:</p>
+              <p className="font-semibold text-gray-800">{cartItemsCount}</p>
+            </div>
+            <div className="flex justify-between mb-4">
+              <p className="text-gray-600">Items Total:</p>
+              <p className="font-semibold text-gray-800">₹ {cartTotal}</p>
+            </div>
+            <div className="flex justify-between mb-4">
+              <p className="text-gray-600">Delivery Fee:</p>
+              <p className="font-semibold text-gray-800">₹ 50.00</p>
+            </div>
+            <div className="flex justify-between mb-4">
+              <p className="text-gray-600">Discount:</p>
+              <p className="font-semibold text-gray-800">-₹ 00.00</p>
+            </div>
+            <div className="border-t-2 border-gray-300 mt-4 pt-4 flex justify-between">
+              <p className="font-semibold text-lg text-gray-800">Total:</p>
+              <p className="font-semibold text-lg text-gray-800">
+                ₹ {cartTotal + 50}
+              </p>
+            </div>
 
-        {/* Secure Payment */}
-        <div className="mt-6 text-center text-gray-600">
-          <p className="text-sm">Secure payment via</p>
-          <p className="font-bold text-gray-800">Razorpay</p>
-        </div>
+            <div className="mt-6 text-center text-gray-600">
+              <p className="text-sm">Secure payment via</p>
+              <p className="font-bold text-gray-800">Razorpay</p>
+            </div>
 
-        {/* Payment Button */}
-        <div className="mt-6 flex justify-center">
-          <button className="w-full bg-green-500 text-white py-1 md:py-3 px-6 rounded-md hover:bg-green-600 transition duration-300" onClick={handlePayment}>
-            Proceed to Payment
-          </button>
-        </div>
-      </div>
+            <div className="mt-6">
+              <button
+                className="w-full bg-green-500 text-white py-2 px-6 rounded-md hover:bg-green-600 transition duration-300"
+                onClick={handlePayment}
+              >
+                Proceed to Payment
+              </button>
+            </div>
+          </div>
     </div>
+      )}
+
+    {showSuccessDiv && (
+        <div className="flex justify-center items-center w-full h-screen">
+          <Confetti
+            width={width}
+            height={height}
+          />
+          <div className="p-8 max-w-lg w-full text-center border-2 border-orange-500 shadow-lg rounded-lg bg-white md:w-6/12">
+            <div className="flex justify-center items-center mb-4">
+              <h1 className="text-3xl font-bold text-orange-500 mb-4">
+                Your Order is Placed!
+              </h1>
+            </div>
+            <p className="text-lg text-gray-700 mb-6">
+              It will take approximately 30 minutes to deliver.
+            </p>
+            <p className="text-sm text-gray-600 mb-6">
+              Wait for the delivery person to arrive and enjoy your order!
+            </p>
+            <p className="text-sm text-gray-600 mb-6">
+              You will redirect to home in 3 seconds.
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
